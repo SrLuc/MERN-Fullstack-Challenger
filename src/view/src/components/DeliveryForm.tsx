ffd5ff668710b4cc38da11e6426cbeebac69eef9
SrLuc <ruanlucenaramos@gmail.com>
2024-01-26 00:01:44 -0300

    style: add style on form delivery

--- src/view/src/components/DeliveryForm.tsx
+++ src/view/src/components/DeliveryForm.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
+import "../css/UIElements.css";
 
 interface DeliveryFormProps {}
 
 const DeliveryForm = ({}: DeliveryFormProps) => {
-
   const handleDeliverySubmit = () => {
     const name = document.getElementById("name") as HTMLInputElement;
     const kg = document.getElementById("kg") as HTMLInputElement;
@@ -48,7 +48,7 @@ const DeliveryForm = ({}: DeliveryFormProps) => {
     <section className="p-5 xl:w-1/4 lg:w-1/4 md:w-1/4 sm:w-full">
       <form
         action="POST"
-        className="flex flex-col "
+        className="flex flex-col h-1/2 justify-evenly "
         onSubmit={() => {
           handleDeliverySubmit();
         }}
@@ -57,44 +57,62 @@ const DeliveryForm = ({}: DeliveryFormProps) => {
           type="text"
           name="name"
           placeholder="Nome do Cliente"
-          className="text-center"
+          className="text-center userInputs"
           id="name"
         />
         <input
           type="text"
           name="Kg"
           placeholder="Peso da Entrega"
-          className="text-center"
+          className="text-center userInputs"
           id="kg"
         />
         <input
           type="text"
           name="address"
           placeholder="Endereço Cliente"
-          className="text-center"
+          className="text-center userInputs"
           id="address"
         />
-        <button onClick={getGeolocation} type="button">
+        <button className="searchButton" onClick={getGeolocation} type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-repeat"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"></path>
+            <path
+              fill-rule="evenodd"
+              d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+            ></path>
+          </svg>
           Buscar
         </button>
-        <div className="flex justify-center">
+        <div className="flex justify-around">
           <input
             type="text"
             placeholder="latitude"
             disabled
-            className="sm:w-1/2 text-center"
+            className="sm:w-2/3 text-center latLong"
             id="lat"
           />
           <input
             type="text"
             placeholder="longitude"
             disabled
-            className="sm:w-1/2 text-center"
+            className="sm:w-2/3 text-center latLong"
             id="lng"
           />
         </div>
-        <button type="submit">Cadastrar Cliente</button>
-        <button type="submit">Resetar Cadastro</button>
+        <button className="styledButton" type="submit">
+          Cadastrar Cliente
+        </button>
+        <button className="styledButton" type="submit">
+          Resetar Cadastro
+        </button>
       </form>
     </section>
   );
