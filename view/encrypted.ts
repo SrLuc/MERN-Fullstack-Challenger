7f844e28c00dce51cdb0077b2d7cea81f8e41418
SrLuc <ruanlucenaramos@gmail.com>
2024-01-28 00:07:59 -0300

    update git ignore

---
+++ view/encrypted.ts
@@ -0,0 +1,6 @@
+const encrypted = {
+  REACT_GEOLOCATION_GOOGLE_API_KEY: "AIzaSyB7Q6umkMcCwXwVb2wl5B4Htdy84fw89F0",
+  ALL_DELIVERIES: "http://localhost:7936/deliveries",
+};
+
+export default encrypted;
