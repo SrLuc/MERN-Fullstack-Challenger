7119d336b69aedaafb547c44e68992fd019ef857
SrLuc <ruanlucenaramos@gmail.com>
2024-01-27 21:46:04 -0300

    refacto: change directories

---
+++ view/src/components/DeliveryTable.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface DeliveryTableProps {}
+
+const DeliveryTable = ({}: DeliveryTableProps) => {
+  const [deliveryDataTable, setDeliveryDataTable] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7936/deliveries")
+      .then((response) => {
+        setDeliveryDataTable(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <section className="teste">
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Rua</th>
+            <th>Cidade</th>
+            <th>País</th>
+            <th>Peso</th>
+            <th>Lat</th>
+            <th>Lng</th>
+          </tr>
+        </thead>
+        <tbody>
+          {deliveryDataTable.map((deliveryData: any) => {
+            return (
+              <tr key={deliveryData.id}>
+                <td>{deliveryData.name}</td>
+                <td>{deliveryData.address.publicPlace}</td>
+                <td>{deliveryData.address.city}</td>
+                <td>{deliveryData.address.country}</td>
+                <td>{deliveryData.kg}</td>
+                <td>{deliveryData.address.location[0]}</td>
+                <td>{deliveryData.address.location[1]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default DeliveryTable;
---
+++ view/src/components/Map.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
+
+interface DeliveryMapProps {
+  children?: React.ReactNode;
+}
+
+const DeliveryMap = ({ children }: DeliveryMapProps) => {
+  const [deliveryMap, setDeliveryMap] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:7936/deliveries")
+      .then((response) => {
+        setDeliveryMap(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <section className="w-full">
+      <div id="map">
+        <MapContainer
+          center={[-14.235, -51.9253]}
+          zoom={5}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {deliveryMap.map((delivery: any) => {
+            return (
+              <Marker
+                key={delivery.id}
+                position={delivery.address.location}
+              ></Marker>
+            );
+          })}
+        </MapContainer>
+      </div>
+    </section>
+  );
+};
+
+export default DeliveryMap;
