805263b04cb9418e5c3b8e78da8f9ede2fe4ace9
SrLuc <ruanlucenaramos@gmail.com>
2024-02-17 02:23:39 -0300

    doc: adding swagger to documentade the Delivery API

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const handleError = require("./middlewares/error");
 const devliveriesRoutes = require("./routes/deliveries-routes");
 const app = express();
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 app.use(express.json());
 app.use(handleError());
 app.use(cors());
 app.use(devliveriesRoutes);
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
